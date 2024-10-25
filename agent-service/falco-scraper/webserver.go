67e7a6a95af10af5ff5e56c44d280a91f616a440
Asher Siddique <ashersiddique127@gmail.com>
2024-10-26 03:35:29 +0500

    feat: Set up single endpoint server for Falco webhook

--- agent-service/falco-scraper/webserver.go
+++ agent-service/falco-scraper/webserver.go
@@ -1,1 +1,27 @@
 package falco_scraper
+
+import (
+	"io"
+	"net/http"
+)
+
+const SERVER_ADDR = ":7744"
+
+func falcoWebhook(w http.ResponseWriter, req *http.Request) {
+	bodyBytes, err := io.ReadAll(req.Body)
+	if err != nil {
+		// log error
+		return
+	}
+	bodyString := string(bodyBytes)
+
+}
+
+func RegisterEndpoint() {
+	http.HandleFunc("/agent-service/falco", falcoWebhook)
+	err := http.ListenAndServe(SERVER_ADDR, nil)
+	if err != nil {
+		// perform logging for error
+		return
+	}
+}
