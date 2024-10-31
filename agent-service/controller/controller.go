5abdaec9752f3df54f4562f286fd293e31a20da2
Asher Siddique <ashersiddique127@gmail.com>
2024-11-01 02:26:21 +0500

    feat: Controller fetching and sending off combined metrics

--- agent-service/controller/controller.go
+++ agent-service/controller/controller.go
@@ -3,13 +3,57 @@ package controller
 import (
 	falcoscraper "agent-service/falco-scraper"
 	nodeexporterscraper "agent-service/node-exporter-scraper"
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 	"strconv"
 	"strings"
 	"time"
 )
 
-const TIME_INTERVAL = int64(4)
+const (
+	TIME_INTERVAL       = int64(4)
+	CENTRAL_SERVER_ADDR = "http://localhost:7744/monitor/agent-service"
+)
+
+type CombinedMetrics struct {
+	Timestamp       string             `json:"timestamp"`
+	Syscalls        map[string]int     `json:"syscalls"`
+	ResourceMetrics map[string]float64 `json:"resource_metrics"`
+}
+
+func sendToCentralServer(data *CombinedMetrics) {
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return
+	}
+
+	req, err := http.NewRequest("POST", CENTRAL_SERVER_ADDR, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{Timeout: 10 * time.Second}
+	resp, err := client.Do(req)
+	if err != nil {
+		return
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			return
+		}
+	}(resp.Body)
+
+	if resp.StatusCode != http.StatusOK {
+		return
+	}
+
+}
 
 func FetchAndCombine() {
 	for {
@@ -23,19 +67,20 @@ func FetchAndCombine() {
 		}
 		if i%TIME_INTERVAL == 0 {
 			for {
-				/*
-					Sleep here for x seconds as we have reached a time interval where
-					sleeping for x seconds will always wake it up at the next correct
-					time of scraping.
-				*/
 				time.Sleep(time.Second * time.Duration(TIME_INTERVAL))
+
 				syscalls := falcoscraper.GetSyscalls()
 				nodeexporterscraper.GetMetrics()
 				resourceMetrics := nodeexporterscraper.GetResourceMetrics()
 
-				// combine and send off
-				fmt.Println(*syscalls, "\n", *resourceMetrics)
-				fmt.Println()
+				combinedData := &CombinedMetrics{
+					Timestamp:       time.Now().UTC().Format(time.RFC3339),
+					Syscalls:        *syscalls,
+					ResourceMetrics: *resourceMetrics,
+				}
+
+				fmt.Println(combinedData)
+				sendToCentralServer(combinedData)
 
 				falcoscraper.ResetSyscalls()
 				nodeexporterscraper.ResetResourceMetrics()
