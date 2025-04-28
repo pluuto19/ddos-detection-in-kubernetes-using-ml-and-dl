4e651bfccfc46b4a2500846b367261d1249902fd
icantkode <ashersiddique127@gmail.com>
2025-04-28 14:23:24 +0500

    Updated Dockerfiles

--- data-script/client/build.sh
+++ data-script/client/build.sh
@@ -2,9 +2,9 @@
 set -e
 
 echo "Starting HTTP attacker image build"
-docker build -t fyp/http-attacker ./http_generator/Dockerfile
+docker build -t fyp/http-attacker ./http_generator/
 echo "Image built successfully"
 
 echo "Starting TCP attacker image build"
-docker build -t fyp/tcp-attacker ./tcp_generator/Dockerfile
+docker build -t fyp/tcp-attacker ./tcp_generator/
 echo "Image built successfully"
